4ef6d95e46fe65ab89ecc189856172a25c4a5479
David Bassey <davidosky1@gmail.com>
2021-03-17 13:17:51 +0100

    Add DOM manipulation code in ui.js file

--- src/ui.js
+++ src/ui.js
@@ -2,13 +2,12 @@ import storage from './storage';
 import Task from './task';
 import Project from './project';
 
-
 const UI = (() => {
   const btnCreateProject = document.getElementById('create-project');
   const projectsUser = document.getElementById('projects-user');
   const currentProject = document.getElementById('current-project');
   const btnAddTask = document.getElementById('add-task');
-  const projectTask = document.getElementById('project-tasks');
+  const projectTasks = document.getElementById('project-tasks');
   const currentTask = document.getElementById('current-task');
   const detailsTask = document.getElementById('detail-task');
   const inputDueDate = document.getElementById('due-date');
@@ -18,7 +17,7 @@ const UI = (() => {
   const btnEdit = document.getElementById('edit-task');
   const btnSave = document.getElementById('save-task');
   const divProjectsName = document.getElementById('projects-names');
-  const divTaskName = Document.getElementById('tasks-names');
+  const divTasksName = document.getElementById('tasks-names');
   const alertMessage = document.querySelector('.alert');
 
   let chosenProject;
@@ -40,8 +39,8 @@ const UI = (() => {
     });
   };
 
-  const renderTask = () => {
-    projectTask.innerHTML = '';
+  const renderTasks = () => {
+    projectTasks.innerHTML = '';
     for (let i = 0; i < chosenProject.tasks.length; i += 1) {
       const div = document.createElement('div');
       const divCheck = document.createElement('div');
@@ -68,7 +67,7 @@ const UI = (() => {
       divCheck.appendChild(checkIsDone);
       div.appendChild(divCheck);
       div.appendChild(span);
-      if (checkIsDone.tasks[i].dueDate !== '') {
+      if (chosenProject.tasks[i].dueDate !== '') {
         spanDate.innerHTML = chosenProject.tasks[i].dueDate;
         div.appendChild(spanDate);
       }
@@ -93,13 +92,14 @@ const UI = (() => {
     renderProjects();
   };
 
+
   const selectedProject = (e) => {
     if (e.target.tagName.toLowerCase() === 'span') {
       currentProject.innerText = e.target.innerText;
       chosenProject = Project(storage.read(e.target.innerText));
       itemActive(projectsUser);
       e.target.parentNode.classList.add('active');
-      renderTask();
+      renderTasks();
     } else if (e.target.classList.contains('remove')) {
       storage.remove(e.target.dataset.name);
       projectTasks.innerHTML = '';
@@ -114,20 +114,114 @@ const UI = (() => {
     inputNameTask.value = '';
   };
 
-  const addTaskToProject = () => {
+
+  const AddTaskToProject = () => {
     cleanInputValues();
     detailsTask.classList.remove('d-none');
     divProjectsName.classList.add('opacity');
+    divTasksName.classList.add('opacity');
     btnSave.classList.remove('d-none');
     btnEdit.classList.add('d-none');
   };
 
   const saveTask = () => {
     const newTask = Task({});
-   newTask.dueDate = inputDueDate.value;
-   newTask.priority = selectPriority.value;
-   newTask.title = inputNameTask.value;
-   newTask.description = inputDescription.value;
-    
-  }
-});
+    newTask.dueDate = inputDueDate.value;
+    newTask.priority = selectPriority.value;
+    newTask.description = inputDescription.value;
+    newTask.title = inputNameTask.value;
+    if (newTask.dueDate !== '' && newTask.priority !== '' && newTask.description !== '' && newTask.title !== '') {
+      if (!chosenProject) chosenProject = defaultProject;
+      chosenProject.tasks.push(newTask);
+      storage.update(chosenProject.title, chosenProject);
+      renderTasks();
+      divProjectsName.classList.remove('opacity');
+      divTasksName.classList.remove('opacity');
+      detailsTask.classList.add('d-none');
+      cleanInputValues();
+    }
+  };
+
+  const selectedTask = (e) => {
+    if (e.target.classList.contains('name-task')) {
+      currentTask.innerText = e.target.innerText;
+      actualTask = Task(chosenProject.findTask(e.target.innerText));
+      inputDueDate.value = actualTask.dueDate;
+      selectPriority.value = actualTask.priority;
+      inputDescription.value = actualTask.description;
+      inputNameTask.value = actualTask.title;
+      btnSave.classList.add('d-none');
+      btnEdit.classList.remove('d-none');
+      itemActive(projectTasks);
+      e.target.parentNode.classList.add('active');
+      detailsTask.classList.remove('d-none');
+    } else if (e.target.classList.contains('delete')) {
+      const chosenTask = chosenProject.tasks.filter(task => task.title !== e.target.dataset.task);
+      chosenProject.tasks = chosenTask;
+      storage.update(chosenProject.title, chosenProject);
+      currentTask.innerText = 'Current Task';
+      detailsTask.classList.add('d-none');
+      renderTasks();
+    } else if (e.target.classList.contains('form-check-input')) {
+      const taskToCheck = e.target.dataset.task;
+      for (let i = 0; i < chosenProject.tasks.length; i += 1) {
+        if (chosenProject.tasks[i].title === taskToCheck) {
+          chosenProject.tasks[i].isDone = !chosenProject.tasks[i].isDone;
+          storage.update(chosenProject.title, chosenProject);
+          renderTasks();
+          return;
+        }
+      }
+    }
+  };
+
+  const editTask = () => {
+    actualTask.dueDate = inputDueDate.value;
+    actualTask.priority = selectPriority.value;
+    actualTask.description = inputDescription.value;
+    if (actualTask.dueDate !== '' && actualTask.priority !== '' && actualTask.description !== '' && inputNameTask.value !== '') {
+      for (let i = 0; i < chosenProject.tasks.length; i += 1) {
+        if (chosenProject.tasks[i].title === actualTask.title) {
+          actualTask.title = inputNameTask.value;
+          chosenProject.tasks[i] = actualTask;
+          storage.update(chosenProject.title, chosenProject);
+          renderTasks();
+          alertMessage.classList.remove('d-none');
+          detailsTask.classList.add('d-none');
+          setTimeout(() => {
+            alertMessage.classList.add('d-none');
+          }, (3 * 1000));
+          return;
+        }
+      }
+    }
+  };
+
+  const createDefaultProject = () => {
+    if (storage.create(defaultProject.title, defaultProject)) {
+      chosenProject = defaultProject;
+    } else {
+      chosenProject = Project(storage.read(defaultProject.title));
+      renderTasks();
+    }
+  };
+
+  const loadListeners = () => {
+    createDefaultProject();
+    renderProjects();
+    btnCreateProject.addEventListener('click', createProject);
+    projectsUser.addEventListener('click', selectedProject);
+    btnAddTask.addEventListener('click', AddTaskToProject);
+    projectTasks.addEventListener('click', selectedTask);
+    btnEdit.addEventListener('click', editTask);
+    btnSave.addEventListener('click', saveTask);
+  };
+
+  return {
+    loadListeners,
+  };
+}
+
+)();
+
+export default UI;
